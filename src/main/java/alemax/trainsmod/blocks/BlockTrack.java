a5f584de4e79540532512af1c24b352be58d1061
AleMaxSNG <alemaxsng@gmail.com>
2018-11-10 20:12:37 +0100

    various stuff finishing most math related things

---
+++ src/main/java/alemax/trainsmod/blocks/BlockTrack.java
@@ -0,0 +1,15 @@
+package alemax.trainsmod.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockTrack extends Block {
+
+	public BlockTrack(Material materialIn) {
+		super(materialIn);
+		
+	}
+	
+	
+	
+}
--- src/main/java/alemax/trainsmod/blocks/tileentities/TileEntityTrackMarker.java
+++ src/main/java/alemax/trainsmod/blocks/tileentities/TileEntityTrackMarker.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.vecmath.Vector2d;
+import javax.vecmath.Vector3d;
 
 import alemax.trainsmod.TrainsMod;
 import alemax.trainsmod.util.TrackType;
+import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
@@ -81,8 +83,19 @@ public class TileEntityTrackMarker extends TileEntity {
 			double distance = Math.sqrt(Math.pow(tileEntity.getPos().getX() - getPos().getX(), 2) + Math.pow(tileEntity.getPos().getZ() - getPos().getZ(), 2));
 			if(distance > 1.9) {
 				List<Vector2d> points = calculateTrack((TileEntityTrackMarker) tileEntity, distance);
+				Vector3d[] points3d = getPointsWithHeight(points, (TileEntityTrackMarker) tileEntity);
 				
+				Vector3d[] nextPoints = getLeftRightPoints(points3d, 1);
+				Vector3d[] next2Points = getLeftRightPoints(points3d, -1);
 				
+				//System.out.println(points.size());
+				//System.out.println(nextPoints.size());
+				
+				for(int i = 0; i < points.size(); i++) {
+					this.world.setBlockState(new BlockPos(nextPoints[i].x, nextPoints[i].y, nextPoints[i].z), Blocks.DIAMOND_BLOCK.getDefaultState());
+					this.world.setBlockState(new BlockPos(next2Points[i].x, next2Points[i].y, next2Points[i].z), Blocks.DIAMOND_BLOCK.getDefaultState());
+					this.world.setBlockState(new BlockPos(points3d[i].x, points3d[i].y, points3d[i].z), Blocks.COAL_BLOCK.getDefaultState());
+				}
 				
 			} else {
 				System.out.println("TO CLOSE TO EACH OTHER");
@@ -93,6 +106,19 @@ public class TileEntityTrackMarker extends TileEntity {
 			//error message
 		}
 	}
+	
+	private Vector3d[] getPointsWithHeight(List<Vector2d> points2d, TileEntityTrackMarker otherTileEntity) {
+		Vector3d[] points3d = new Vector3d[points2d.size()];
+		double currentHeighth = this.pos.getY() + (this.height / 16.0) + 1;
+		double finalHeighth = otherTileEntity.pos.getY() + (otherTileEntity.height / 16.0) + 1;
+		double heightDifference = (finalHeighth - currentHeighth) / (points3d.length - 1);
+		
+		for(int i = 0; i < points3d.length; i++) {
+			points3d[i] = new Vector3d(points2d.get(i).x, currentHeighth + heightDifference * i , points2d.get(i).y);
+		}
+		return points3d;
+	}
+	
 	
 	private List<Vector2d> calculateTrack(TileEntityTrackMarker otherTileEntity, double distance) {
 		synchronizeAngle(otherTileEntity);
@@ -236,10 +262,19 @@ public class TileEntityTrackMarker extends TileEntity {
 			}
 			return finalPoints;
 		} else {
-			Vector2d intersect = getLineIntersection(new Vector2d(this.pos.getX() + 0.5, this.pos.getZ() + 0.5), new Vector2d(this.pos.getX() + 0.5 - Math.sin(Math.toRadians(this.realAngle)), this.pos.getZ() + 0.5 + Math.cos(Math.toRadians(this.realAngle))), new Vector2d(otherTileEntity.pos.getX() + 0.5, otherTileEntity.pos.getZ() + 0.5), new Vector2d(otherTileEntity.pos.getX() + 0.5 - Math.sin(Math.toRadians(otherTileEntity.realAngle)), otherTileEntity.pos.getZ() + 0.5 + Math.cos(Math.toRadians(otherTileEntity.realAngle))));
-			double firstDistance = Math.sqrt(Math.pow(intersect.x - (this.pos.getX() + 0.5), 2) + Math.pow(intersect.y - (this.pos.getZ() + 0.5), 2));
-			double secondDistance = Math.sqrt(Math.pow(intersect.x - (otherTileEntity.pos.getX() + 0.5), 2) + Math.pow(intersect.y - (otherTileEntity.pos.getZ() + 0.5), 2));
-			
+			Vector2d intersect = null;
+			double firstDistance = 0;
+			double secondDistance = 0;
+			try {
+				intersect = getLineIntersection(new Vector2d(this.pos.getX() + 0.5, this.pos.getZ() + 0.5), new Vector2d(this.pos.getX() + 0.5 - Math.sin(Math.toRadians(this.realAngle)), this.pos.getZ() + 0.5 + Math.cos(Math.toRadians(this.realAngle))), new Vector2d(otherTileEntity.pos.getX() + 0.5, otherTileEntity.pos.getZ() + 0.5), new Vector2d(otherTileEntity.pos.getX() + 0.5 - Math.sin(Math.toRadians(otherTileEntity.realAngle)), otherTileEntity.pos.getZ() + 0.5 + Math.cos(Math.toRadians(otherTileEntity.realAngle))));
+				firstDistance = Math.sqrt(Math.pow(intersect.x - (this.pos.getX() + 0.5), 2) + Math.pow(intersect.y - (this.pos.getZ() + 0.5), 2));
+				secondDistance = Math.sqrt(Math.pow(intersect.x - (otherTileEntity.pos.getX() + 0.5), 2) + Math.pow(intersect.y - (otherTileEntity.pos.getZ() + 0.5), 2));
+			} catch (Exception e) {
+				
+				return null;
+			}
+				
+				
 			if(secondDistance > firstDistance) {
 				Vector2d sideVector = getLeftRightVector(firstSide);
 				Vector2d otherLineVector = new Vector2d(Math.sin(Math.toRadians(otherTileEntity.realAngle)), -Math.cos(Math.toRadians(otherTileEntity.realAngle)));
@@ -254,6 +289,8 @@ public class TileEntityTrackMarker extends TileEntity {
 				Vector2d middlePoint = new Vector2d(this.pos.getX() + 0.5 + sideVector.x * radius, this.pos.getZ() + 0.5 + sideVector.y * radius);
 				Vector2d perp = getPerpendicularPoint(new Vector2d(otherTileEntity.pos.getX() + 0.5, otherTileEntity.pos.getZ() + 0.5), new Vector2d(otherTileEntity.pos.getX() + 0.5 - Math.sin(Math.toRadians(otherTileEntity.realAngle)), otherTileEntity.pos.getZ() + 0.5 + Math.cos(Math.toRadians(otherTileEntity.realAngle))), middlePoint);
 				
+				System.out.println(perp.x + "\t" + perp.y);
+				
 				double angleDifference = this.realAngle - (otherTileEntity.realAngle + 180) % 360;
 				angleDifference = Math.abs(angleDifference);
 				if(angleDifference > 180) angleDifference = 360 - angleDifference;
@@ -264,7 +301,7 @@ public class TileEntityTrackMarker extends TileEntity {
 				int circleSteps = (int) Math.round(Math.floor(circleScaled));
 				
 				Vector2d currentPoint = new Vector2d(-Math.sin(Math.toRadians(this.realAngle)) * 0.5 * scaleFactor, Math.cos(Math.toRadians(this.realAngle)) * 0.5 * scaleFactor);
-				Vector2d aimedPoint = new Vector2d(perp.x - (this.pos.getX()), perp.y - (this.pos.getZ()));
+				Vector2d aimedPoint = new Vector2d(perp.x - (this.pos.getX() + 0.5), perp.y - (this.pos.getZ() + 0.5));
 				double currentAngle = this.realAngle;
 				double angleSteps = angleDifference / (circleSteps + 1);
 				byte incrementFactor = 1;
@@ -292,12 +329,13 @@ public class TileEntityTrackMarker extends TileEntity {
 				
 				ArrayList<Vector2d> linePoints = new ArrayList<>();
 				
-				int steps = (int) Math.round(secondDistance);
+				Vector2d lineDirection = new Vector2d(otherTileEntity.pos.getX() + 0.5 - perp.x, otherTileEntity.pos.getZ() + 0.5 - perp.y);
+				double lineLength = lineDirection.length();
+				int steps = (int) Math.round(lineLength);
 				if(steps == 0) steps = 1;
-				Vector2d lineDirection = new Vector2d(otherTileEntity.pos.getX() + 0.5 - intersect.x, otherTileEntity.pos.getZ() + 0.5 - intersect.y);
 				lineDirection.scale(1.0 / steps);
 				for(int i = 1; i < steps + 1; i++) {
-					linePoints.add(new Vector2d(intersect.x + (lineDirection.x * i), intersect.y + (lineDirection.y * i)));
+					linePoints.add(new Vector2d(perp.x + (lineDirection.x * i), perp.y + (lineDirection.y * i)));
 				}
 				
 				ArrayList<Vector2d> finalPoints = new ArrayList<>();
@@ -315,15 +353,16 @@ public class TileEntityTrackMarker extends TileEntity {
 				Vector2d sideVector = otherTileEntity.getLeftRightVector(secondSide);
 				Vector2d otherLineVector = new Vector2d(Math.sin(Math.toRadians(this.realAngle)), -Math.cos(Math.toRadians(this.realAngle)));
 				double alpha = 0;
-				if(firstSide < 1) alpha = getCounterClockwiseAngle(otherLineVector, sideVector);
-				else alpha = getCounterClockwiseAngle(sideVector, otherLineVector);
+				if(firstSide < 1) alpha = getCounterClockwiseAngle(sideVector, otherLineVector);
+				else alpha = getCounterClockwiseAngle(otherLineVector, sideVector);
 				alpha *= -1;
 				if(alpha < 0) alpha += 360;
 				
-				double radius = Math.cos(Math.toRadians(alpha)) * secondDistance / (1 - Math.sin(Math.toRadians(alpha)));
+				double radius = Math.cos(Math.toRadians(alpha)) * secondDistance / (1 - Math.sin(Math.toRadians(alpha))); //ich hoffe doch..
 				sideVector.normalize();
 				Vector2d middlePoint = new Vector2d(otherTileEntity.pos.getX() + 0.5 + sideVector.x * radius, otherTileEntity.pos.getZ() + 0.5 + sideVector.y * radius);
 				Vector2d perp = getPerpendicularPoint(new Vector2d(this.pos.getX() + 0.5, this.pos.getZ() + 0.5), new Vector2d(this.pos.getX() + 0.5 - Math.sin(Math.toRadians(this.realAngle)), this.pos.getZ() + 0.5 + Math.cos(Math.toRadians(this.realAngle))), middlePoint);
+				//System.out.println(radius);
 				
 				double angleDifference = otherTileEntity.realAngle - (this.realAngle + 180) % 360;
 				angleDifference = Math.abs(angleDifference);
@@ -335,7 +374,7 @@ public class TileEntityTrackMarker extends TileEntity {
 				int circleSteps = (int) Math.round(Math.floor(circleScaled));
 				
 				Vector2d currentPoint = new Vector2d(-Math.sin(Math.toRadians(otherTileEntity.realAngle)) * 0.5 * scaleFactor, Math.cos(Math.toRadians(otherTileEntity.realAngle)) * 0.5 * scaleFactor);
-				Vector2d aimedPoint = new Vector2d(perp.x - (otherTileEntity.pos.getX()), perp.y - (otherTileEntity.pos.getZ()));
+				Vector2d aimedPoint = new Vector2d(perp.x - (otherTileEntity.pos.getX() + 0.5), perp.y - (otherTileEntity.pos.getZ() + 0.5));
 				double currentAngle = otherTileEntity.realAngle;
 				double angleSteps = angleDifference / (circleSteps + 1);
 				byte incrementFactor = 1;
@@ -363,18 +402,19 @@ public class TileEntityTrackMarker extends TileEntity {
 				
 				ArrayList<Vector2d> linePoints = new ArrayList<>();
 				
-				int steps = (int) Math.round(firstDistance);
+				Vector2d lineDirection = new Vector2d(this.pos.getX() + 0.5 - perp.x, this.pos.getZ() + 0.5 - perp.y);
+				double lineLength = lineDirection.length();
+				int steps = (int) Math.round(lineLength);
 				if(steps == 0) steps = 1;
-				Vector2d lineDirection = new Vector2d(this.pos.getX() + 0.5 - intersect.x, this.pos.getZ() + 0.5 - intersect.y);
 				lineDirection.scale(1.0 / steps);
 				for(int i = 1; i < steps + 1; i++) {
-					linePoints.add(new Vector2d(intersect.x + (lineDirection.x * i), intersect.y + (lineDirection.y * i)));
+					linePoints.add(new Vector2d(perp.x + (lineDirection.x * i), perp.y + (lineDirection.y * i)));
 				}
 				
 				ArrayList<Vector2d> finalPoints = new ArrayList<>();
-				finalPoints.add(new Vector2d(this.pos.getX() + 0.5, this.pos.getZ() + 0.5));
+				finalPoints.add(new Vector2d(otherTileEntity.pos.getX() + 0.5, otherTileEntity.pos.getZ() + 0.5));
 				for(int i = 0; i < circlePoints.size(); i++) {
-					finalPoints.add(new Vector2d(this.pos.getX() + 0.5 + circlePoints.get(i).x, this.pos.getZ() + 0.5 + circlePoints.get(i).y));
+					finalPoints.add(new Vector2d(otherTileEntity.pos.getX() + 0.5 + circlePoints.get(i).x, otherTileEntity.pos.getZ() + 0.5 + circlePoints.get(i).y));
 				}
 				
 				for(int i = 0; i < linePoints.size(); i++) {
@@ -396,9 +436,19 @@ public class TileEntityTrackMarker extends TileEntity {
 	}
 	
 	private Vector2d getPerpendicularPoint(Vector2d linePoint1, Vector2d linePoint2, Vector2d point) {
+		
+		
 		double k = ((linePoint2.y - linePoint1.y) * (point.x - linePoint1.x) - (linePoint2.x - linePoint1.x) * (point.y - linePoint1.y)) / (Math.pow(linePoint2.y - linePoint1.y, 2) + Math.pow(linePoint2.x - linePoint1.x, 2));
 		double x = point.x - k * (linePoint2.y - linePoint1.y);
 		double y = point.y + k * (linePoint2.x - linePoint1.x);
+		
+		
+		/*
+		double k =  ((point.x - linePoint1.x) * (linePoint2.x - linePoint1.x) + (point.y - linePoint1.y) * (linePoint2.y - linePoint1.y)) / (Math.pow(linePoint2.x - linePoint1.x, 2) + Math.pow(linePoint2.y - linePoint1.y, 2));
+		double x = linePoint1.x - k * (linePoint2.x - linePoint1.x);
+		double y = linePoint1.y + k * (linePoint2.y - linePoint1.y);
+	*/
+
 		return new Vector2d(x, y);
 	}
 	
@@ -484,8 +534,24 @@ public class TileEntityTrackMarker extends TileEntity {
 		return middleLine.angle(lineToTE);
 	}
 	
-	private List<Vector2d> getLeftRightPoints(List<Vector2d> points, double direction) {
-		
+	private Vector3d[] getLeftRightPoints(Vector3d[] points, double direction) {
+		Vector3d[] newPoints = new Vector3d[points.length];
+		if(points.length > 0) {
+			Vector2d[] currentPoints = {new Vector2d(2 * points[0].x - points[1].x, 2 * points[0].z - points[1].z), new Vector2d(points[0].x, points[0].z) , new Vector2d(points[1].x, points[1].z)};
+			Vector2d point = getLeftRightPoint(currentPoints, direction);
+			newPoints[0] = new Vector3d(point.x, points[0].y, point.y);
+		}
+		for(int i = 1; i < points.length - 1; i++) {
+			Vector2d[] currentPoints = {new Vector2d(points[i - 1].x, points[i - 1].z), new Vector2d(points[i].x, points[i].z), new Vector2d(points[i + 1].x, points[i + 1].z)};
+			Vector2d point = getLeftRightPoint(currentPoints, direction);
+			newPoints[i] = new Vector3d(point.x, points[i].y, point.y);
+		}
+		if(points.length > 1) {
+			Vector2d[] currentPoints = {new Vector2d(points[points.length - 2].x, points[points.length - 2].z), new Vector2d(points[points.length - 1].x, points[points.length - 1].z), new Vector2d(2 * points[points.length - 1].x - points[points.length - 2].x, 2 * points[points.length - 1].z - points[points.length - 2].z)};
+			Vector2d point = getLeftRightPoint(currentPoints, direction);
+			newPoints[points.length - 1] = new Vector3d(point.x, points[points.length - 1].y, point.y);
+		}
+		return newPoints;
 	}
 	
 	private Vector2d getLeftRightPoint(Vector2d[] point, double direction) {
@@ -494,7 +560,7 @@ public class TileEntityTrackMarker extends TileEntity {
 			Vector2d bc = new Vector2d(point[2].x - point[1].x, point[2].y - point[1].y);
 			double angleDifference = getCounterClockwiseAngle(ab, bc);
 			double angle = getAngleBetweenPoints(point[1], point[0]);
-			double newAngle = angle + (angleDifference / 2.0);
+			double newAngle = angle - (angleDifference / 2.0);
 			return new Vector2d(point[1].x - Math.sin(Math.toRadians(newAngle)) * direction, point[1].y + Math.cos(Math.toRadians(newAngle)) * direction);
 		}
 		return null;
