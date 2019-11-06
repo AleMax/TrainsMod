29a4b213f12cb49c35fb0c2fd6847c873d735751
AleMax <alemaxsng@gmail.com>
2019-11-06 21:03:38 +0100

    initial fabric commit

---
+++ src/main/java/alemax/trainsmod/TrainsMod.java
@@ -0,0 +1,11 @@
+package alemax.trainsmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TrainsMod implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+		System.out.println("Starting TrainsMod");
+	}
+}
