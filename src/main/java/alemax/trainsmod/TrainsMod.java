a87d516f75e3f091da6cb230f39368d29b2d9c2b
AleMax <alemaxsng@gmail.com>
2019-11-06 21:30:26 +0100

    Added ItemGroup

--- src/main/java/alemax/trainsmod/TrainsMod.java
+++ src/main/java/alemax/trainsmod/TrainsMod.java
@@ -1,11 +1,25 @@
 package alemax.trainsmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 
 public class TrainsMod implements ModInitializer {
+
+	public static final String modid = "trainsmod";
+	public static final String modName = "TrainsMod";
+	public static final String modVersion = "0.2.0";
 
 	@Override
 	public void onInitialize() {
 		System.out.println("Starting TrainsMod");
 	}
+
+	//ItemGroups:
+	public static final ItemGroup ITEM_GROUP_TRAINSMOD = FabricItemGroupBuilder.build(new Identifier("trainsmod", "general"), () -> new ItemStack(Blocks.POWERED_RAIL));
+
+
 }
