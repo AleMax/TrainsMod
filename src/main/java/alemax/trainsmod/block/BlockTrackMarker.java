ca866a0662bfd42fd2e42f71dc0e9c12b75e4e75
AleMax <alemaxsng@gmail.com>
2019-11-07 19:56:40 +0100

    Added textures

--- src/main/java/alemax/trainsmod/block/BlockTrackMarker.java
+++ src/main/java/alemax/trainsmod/block/BlockTrackMarker.java
@@ -7,7 +7,7 @@ public class BlockTrackMarker extends TMBlock {
 
     public BlockTrackMarker() {
         super(Settings.of(Material.STONE));
-        this.name = "block_track_marker";
+        this.name = "track_marker";
         this.itemGroup = TMItemGroups.ITEM_GROUP_TRAINSMOD;
     }
 
