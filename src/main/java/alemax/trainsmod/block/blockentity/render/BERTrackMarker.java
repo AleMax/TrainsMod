da12c7eaec06ab315b311ea33073de8dae107983
Julian Dunskus <julian.dunskus@gmail.com>
2019-11-23 00:07:36 +0100

    fixed triangle vertex order
    must have messed this up in the final cleanup; this causes it to render from the top rather than (mistakenly) from the bottom

--- src/main/java/alemax/trainsmod/block/blockentity/render/BERTrackMarker.java
+++ src/main/java/alemax/trainsmod/block/blockentity/render/BERTrackMarker.java
@@ -27,8 +27,8 @@ public class BERTrackMarker extends BlockEntityRenderer<BlockEntityTrackMarker> 
 		builder.begin(GL11.GL_TRIANGLES, VertexFormats.POSITION);
 		
 		builder.vertex(+0.0, 0, +0.25).next();
-		builder.vertex(-0.1, 0, -0.25).next();
 		builder.vertex(+0.1, 0, -0.25).next();
+		builder.vertex(-0.1, 0, -0.25).next();
 		
 		GlStateManager.color3f(0, 0, 0);
 		tessellator.draw();
